c2f7a84e0f267c7a36ef7f907e96b7220d11f869
Heasds <antonruckov@gmail.com>
2023-05-25 03:10:17 +0300

    Rename some classes and clear code

--- Assets/ConnectionManager.cs
+++ Assets/ConnectionManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Test : MonoBehaviour
+public class ConnectionManager : MonoBehaviour
 {
     public WeatherData weatherData;
+
     private string url = "https://api.openweathermap.org/data/2.5/weather?lat=55.751244&lon=37.618423&appid=6e158cda558fb62251ab77306ba0c1b0&units=metric";
+
     private void Awake()
     {
         StartCoroutine(GetWeatherData());
@@ -18,12 +19,13 @@ public class Test : MonoBehaviour
         www.downloadHandler = new DownloadHandlerBuffer();
 
         yield return www.SendWebRequest();
-        GetWeather(www.downloadHandler.text);
+
+        if (www.result != UnityWebRequest.Result.Success) Debug.LogError(www.error);
+        else GetWeather(www.downloadHandler.text);
     }
 
     private void GetWeather(string _jsonData)
     {
         weatherData = JsonUtility.FromJson<WeatherData>(_jsonData);
-        Debug.Log(_jsonData);
     }
 }
